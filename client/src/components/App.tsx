098419a50dc64dcda5d967450b2706fb8e4c3b00
Giovanni Schenato <gischenato@gmail.com>
2022-11-25 13:01:39 -0300

    random id with uuid

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -4,10 +4,10 @@ import Login from './Login';
 function App() {
   const [userId, setUserId] = useState('');
 
-  return (<>
+  return (<div className='h-screen'>
     {userId}
     <Login onIdSubmit={setUserId}/>
-  </>);
+  </div>);
 }
 
 export default App;
--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,38 +1,40 @@
 import React, {useRef} from 'react'
+import {v4 as uuidV4 } from 'uuid'
 
 interface LoginProps {
-  onIdSubmit: (id: string) => void
+  onIdSubmit: React.Dispatch<React.SetStateAction<string>>
 }
 
 export default function Login({ onIdSubmit }: LoginProps) {
-  // create a ref to the input element
   const idRef = useRef<HTMLInputElement>(null)
 
-  // prevent default form submission
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     if (!idRef.current) return
-
+    
     onIdSubmit(idRef.current.value)
     idRef.current.value = ''
   }
 
+  const createNewId = () => {
+    onIdSubmit(old => uuidV4().toString())
+  }
+
   return (
-    <div className='flex items-center justify-center h-screen'>
-      <div>
-        <form className='flex flex-col gap-4 border-red-300 border-solid border-2 rounded-lg p-10'
-              onSubmit={handleSubmit}>
+    <div className='flex items-center justify-center h-full max-h-fit'>
+      <div className='flex flex-col gap-4 border-red-300 border-solid border-2 rounded-lg p-10'>
+        <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
           <input className='border-red-300  border-solid border-2 rounded-lg p-2'
                  type="text" placeholder="Enter your Id"  ref={idRef}/>
           <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' 
                   type="submit">
-                    Login
-          </button>
-          <button className='bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded' 
-                  type="submit">
-                    Create a new ID
+                  Login
           </button>
         </form>
+          <button className='bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded' 
+                  onClick={createNewId}>
+                  Create a new ID
+          </button>
       </div>
     </div>
   )
